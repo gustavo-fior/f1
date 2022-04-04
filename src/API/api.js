a58501a1e9847e79e968d2b28cfbb42056cfe175
Gustavo Fior <gustavo_fior@outlook.com>
2022-04-03 21:01:29 -0300

    Alterando endpoint

--- src/API/api.js
+++ src/API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const cadastrarUsuario = async (email, nome) => {
-    await axios.post("https://formula1store.herokuapp.com", {
+    await axios.post("https://formula1store.herokuapp.com/email", {
         email: email,
         nome: nome
     });
